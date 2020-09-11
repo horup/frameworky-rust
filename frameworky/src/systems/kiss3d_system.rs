0a486ad635a386e96e69f6e697c67e29c6180226
horup <sah@sah.dk>
2020-09-11 09:03:11 +0200

    updated

--- frameworky/src/systems/kiss3d_system.rs
+++ frameworky/src/systems/kiss3d_system.rs
@@ -2,7 +2,6 @@ use nalgebra::Point3;
 use kiss3d::{window::Window, light::Light, ncollide3d::math::Translation, ncollide3d::math::Vector, camera::ArcBall};
 
 use crate::{SimpleSystem, Context};
-
 pub struct Kiss3DSystem
 {
     window:Window,
@@ -32,8 +31,21 @@ impl SimpleSystem for Kiss3DSystem
 {
     fn once(&mut self, context:&mut Context) {
         let w = &mut self.window;
-       
+        let max = 10;
+        for y in 0..max
         {
+            for x in 0..max
+            {
+                let col = || rand::random::<f32>();
+                
+                let mut c = w.add_cube(1.0, 1.0, 1.0);
+                c.set_color(col(), col(), col());
+                c.set_local_translation(
+                    Translation::new(x as f32 - max as f32/2.0, 0.0, y as f32 - max as f32/2.0));
+            }
+        }
+       
+     /*   {
             let mut c = w.add_cube(1.0, 1.0, 1.0);
             c.set_color(1.0, 1.0, 1.0);
         }
@@ -41,7 +53,7 @@ impl SimpleSystem for Kiss3DSystem
             let mut c = w.add_cube(1.0, 1.0, 1.0);
             c.set_color(1.0, 1.0, 1.0);
             c.set_local_translation(Translation::new(2.0, 0.0, 0.0));
-        }
+        }*/
 
        // w.set_light(Light::StickToCamera);
     }
