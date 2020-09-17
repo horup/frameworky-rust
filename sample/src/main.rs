e33c10773988b99934b23b2c18f7a87063b67caa
horup <sah@sah.dk>
2020-09-17 15:16:18 +0200

    added continously spawn

--- sample/src/main.rs
+++ sample/src/main.rs
@@ -5,25 +5,43 @@ use components::Shape;
 use rand::random;
 use systems::{Kiss3DSystem, BodySystem};
 use frameworky::*;
-use events::MouseButtonDown;
+use events::{MouseButtonDown, MouseButtonUp};
 
 #[derive(Debug, Default)]
-struct ClickSystem {}
+struct ClickSystem
+{
+    spawn:bool
+}
+
 impl SimpleSystem for ClickSystem
 {
-    fn execute(&mut self, context:&mut Context, event:&dyn Any)
+    fn execute(&mut self, _context:&mut Context, event:&dyn Any)
     {
-        let o:Option<&MouseButtonDown> = event.downcast_ref();
-        if let Some(_up) = o {
-            for i in 0..10 {
-                let a = 0.1;
-                let x = random::<f32>() * a;
-                let y = 5.0 + i as f32;
-                let z = random::<f32>() * a;
-        
-                let ball = (Transform::new(x, y, z), Body::default());
-                context.world.push(ball);
+        {
+            let o:Option<&MouseButtonDown> = event.downcast_ref();
+            if let Some(_up) = o {
+                self.spawn = true;
             }
+        }
+
+        {
+            let o:Option<&MouseButtonUp> = event.downcast_ref();
+            if let Some(_up) = o {
+                self.spawn = false;
+            }
+        }
+    }
+
+    fn update(&mut self, context:&mut Context) {
+        if self.spawn 
+        {
+            let a = 0.1;
+            let x = random::<f32>() * a;
+            let y = 10.0 as f32;
+            let z = random::<f32>() * a;
+    
+            let ball = (Transform::new(x, y, z), Body::default());
+            context.world.push(ball);
         }
     }
 }
