b79fccd7e60df8bfe587d63cb3204597861f5e29
Horup <sah@sah.dk>
2020-09-23 21:01:39 +0200

    updated builds

--- build_deploy.cmd
+++ build_deploy.cmd
@@ -1,7 +1,13 @@
 cargo install wasm-pack
 wasm-pack build examples/balls --target web
+
 git add -f examples/balls/pkg
 git stash
+
+git fetch --all
 git checkout --track origin/pages
+git rm examples/balls/pkg -r
+git add .
+git commit -m "cleanup"
 git stash pop
 git commit -m "updated"
