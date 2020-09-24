7ec61fa9176811c271a71e08c861bc758f411c6c
Horup <sah@sah.dk>
2020-09-24 11:42:28 +0200

    added del .gitignore

--- wasm_build.cmd
+++ wasm_build.cmd
@@ -3,4 +3,5 @@ cargo install wasm-pack
 rem Balls example
 wasm-pack build examples/balls --release
 xcopy /Y examples\balls\pkg\*.* public\balls\pkg\
+del public\balls\pkg\.gitignore
 copy /Y examples\balls\index.html public\balls\index.html
