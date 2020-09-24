6bdc0afd2720c37f1040246b676abdddee96a3bb
Horup <sah@sah.dk>
2020-09-24 13:34:58 +0200

    fixed target

--- wasm_build.cmd
+++ wasm_build.cmd
@@ -1,7 +1,7 @@
 cargo install wasm-pack
 
 rem Balls example
-wasm-pack build examples/balls --release
+wasm-pack build examples/balls --release --target web
 xcopy /Y examples\balls\pkg\*.* public\balls\pkg\
 del public\balls\pkg\.gitignore
 copy /Y examples\balls\index.html public\balls\index.html
